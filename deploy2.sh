b64d19489a4d494f24ca7998d8e6cd8fe64f981d
KalaayPT <contact@kalaay.dev>
2024-08-06 22:14:01 +0200

    going to commit sudoku

--- deploy2.sh
+++ deploy2.sh
@@ -10,27 +10,6 @@ NODE_MODULES_FILE_1="$NODE_MODULES_DIR/VPLocalSearchBox.vue"
 CUSTOM_FILE_2="$CUSTOM_DIR/VPNavBarSearchButton.vue"
 NODE_MODULES_FILE_2="$NODE_MODULES_DIR/VPNavBarSearchButton.vue"
 
-# Create backup directory if it doesn't exist
-mkdir -p $CUSTOM_DIR
-
-# Backup the custom files if they exist
-if [ -f "$NODE_MODULES_FILE_1" ]; then
-    cp "$NODE_MODULES_FILE_1" "$CUSTOM_FILE_1"
-    echo "Backup of VPLocalSearchBox.vue created."
-else
-    echo "VPLocalSearchBox.vue not found in node_modules, skipping backup."
-fi
-
-if [ -f "$NODE_MODULES_FILE_2" ]; then
-    cp "$NODE_MODULES_FILE_2" "$CUSTOM_FILE_2"
-    echo "Backup of VPNavBarSearchButton.vue created."
-else
-    echo "VPNavBarSearchButton.vue not found in node_modules, skipping backup."
-fi
-
-# Run npm install
-/opt/plesk/node/7/bin/npm install
-
 # Restore the custom files from backup
 if [ -f "$CUSTOM_FILE_1" ]; then
     cp "$CUSTOM_FILE_1" "$NODE_MODULES_FILE_1"
@@ -47,7 +26,3 @@ else
 fi
 
 echo "Deployment script executed successfully."
-
-
-# build website
-/opt/plesk/node/7/bin/npm run docs:build
