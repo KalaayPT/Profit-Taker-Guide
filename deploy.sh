a688fd78f2a6b5a0b77d62f9d4882faf13d80c1a
KalaayPT <contact@kalaay.dev>
2024-08-06 22:26:19 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,27 @@ NODE_MODULES_FILE_1="$NODE_MODULES_DIR/VPLocalSearchBox.vue"
 CUSTOM_FILE_2="$CUSTOM_DIR/VPNavBarSearchButton.vue"
 NODE_MODULES_FILE_2="$NODE_MODULES_DIR/VPNavBarSearchButton.vue"
 
+# Create backup directory if it doesn't exist
+mkdir -p $CUSTOM_DIR
+
+# Backup the custom files if they exist
+if [ -f "$NODE_MODULES_FILE_1" ]; then
+    cp "$NODE_MODULES_FILE_1" "$CUSTOM_FILE_1"
+    echo "Backup of VPLocalSearchBox.vue created."
+else
+    echo "VPLocalSearchBox.vue not found in node_modules, skipping backup."
+fi
+
+if [ -f "$NODE_MODULES_FILE_2" ]; then
+    cp "$NODE_MODULES_FILE_2" "$CUSTOM_FILE_2"
+    echo "Backup of VPNavBarSearchButton.vue created."
+else
+    echo "VPNavBarSearchButton.vue not found in node_modules, skipping backup."
+fi
+
+# Run npm install
+/opt/plesk/node/22/bin/npm install
+
 # Restore the custom files from backup
 if [ -f "$CUSTOM_FILE_1" ]; then
     cp "$CUSTOM_FILE_1" "$NODE_MODULES_FILE_1"
@@ -26,3 +47,6 @@ else
 fi
 
 echo "Deployment script executed successfully."
+
+# build website
+/opt/plesk/node/22/bin/npm run docs:build
